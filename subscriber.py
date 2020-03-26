b8c89b4245b420b00cd81c53e37acbe9dd409c4b
aj132608 <32584157+aj132608@users.noreply.github.com>
2020-03-25 22:19:38 -0500

    Made the subscriber poll indefinitely.

--- subscriber.py
+++ subscriber.py
@@ -1,4 +1,5 @@
 from google.cloud import pubsub_v1
+import time
 
 
 if __name__ == "__main__":
@@ -8,27 +9,30 @@ if __name__ == "__main__":
 
     print(f"Listening for messages on {subscription_name}")
     print("...")
-    try:
-        subscriber = pubsub_v1.SubscriberClient()
-
-        subscription_path = subscriber.subscription_path(
-            project_id, subscription_name
-        )
-
-
-        def callback(message):
-            print(f"Received message: {message.data}")
-            message.ack()
-
-
-        streaming_pull_future = subscriber.subscribe(
-            subscription_path, callback=callback
-        )
-
+    while True:
         try:
-            streaming_pull_future.result(timeout=0)
-        except Exception as e:  # noqa
-            print(f"Exception: {e}")
-            streaming_pull_future.cancel()
-    except KeyboardInterrupt:
-        print("Closing Subscriber.")
+            subscriber = pubsub_v1.SubscriberClient()
+
+            subscription_path = subscriber.subscription_path(
+                project_id, subscription_name
+            )
+
+
+            def callback(message):
+                print(f"Received message: {message.data}")
+                message.ack()
+
+
+            streaming_pull_future = subscriber.subscribe(
+                subscription_path, callback=callback
+            )
+
+            try:
+                streaming_pull_future.result(timeout=0)
+            except Exception as e:  # noqa
+                # print(f"Exception: {e}")
+                streaming_pull_future.cancel()
+        except KeyboardInterrupt:
+            print("Closing Subscriber.")
+
+        time.sleep(5)
